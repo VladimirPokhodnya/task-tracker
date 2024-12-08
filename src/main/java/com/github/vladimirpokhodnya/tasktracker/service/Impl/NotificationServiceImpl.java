1dc82bd9a423ab8684c04e7449efc7eaed8dcab9
Владимир Походня <pokhodnya-ngs.ru@yandex.ru>
2024-12-08 20:12:08 +0700

    Added email sending

--- src/main/java/com/github/vladimirpokhodnya/tasktracker/service/Impl/NotificationServiceImpl.java
+++ src/main/java/com/github/vladimirpokhodnya/tasktracker/service/Impl/NotificationServiceImpl.java
@@ -1,32 +1,49 @@
 package com.github.vladimirpokhodnya.tasktracker.service.Impl;
 
-import com.github.vladimirpokhodnya.tasktracker.service.MailSenderService;
 import com.github.vladimirpokhodnya.tasktracker.service.NotificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
     private static final Logger logger = LoggerFactory.getLogger(NotificationServiceImpl.class.getName());
 
-    @Value("${spring.email.from-address}")
-    private String mailFrom;
+    @Value("${spring.mail.username}")
+    private String to;
 
-    private final MailSenderService service;
+    @Value("${email.from-address}")
+    private String from;
 
-    public NotificationServiceImpl(MailSenderService service) {
-        this.service = service;
+    private final JavaMailSender mailSender;
+
+    public NotificationServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
     }
 
-    public void sendNotification(String taskId, String newStatus) {
-        service.send(
-                mailFrom,
-                "Change of status",
-                "Task ID = " + taskId + " has its status updated to " + newStatus
-        );
 
-        logger.info("Notification email sent for Task ID: " + taskId + " with new status: " + newStatus);
+    public void sendNotification(String taskId, String newStatus) {
+        logger.info("Notification for Task ID: " + taskId + " with new status: " + newStatus);
+        sendEmail("Notification for Task ID: " + taskId + " with new status: " + newStatus);
+    }
+
+
+    private void sendEmail(String msg) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setFrom(from);
+            message.setTo(to);
+            message.setSubject(msg);
+            message.setText(msg);
+
+            mailSender.send(message);
+            logger.info("Email sent to: <" + to + ">");
+        } catch (Exception e) {
+            logger.error("Error while sending mail", e);
+        }
     }
 }
--- src/main/java/com/github/vladimirpokhodnya/tasktracker/service/MailSenderService.java
+++
@@ -1,7 +0,0 @@
-package com.github.vladimirpokhodnya.tasktracker.service;
-
-public interface MailSenderService {
-
-    void send(String to, String subject, String body);
-
-}
