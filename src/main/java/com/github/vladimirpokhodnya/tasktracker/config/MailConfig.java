9f675d0866751fa6afe06f99ebe796e194106b9a
Владимир Походня <pokhodnya-ngs.ru@yandex.ru>
2024-12-11 11:08:17 +0700

    Added ConfigurationProperties

---
+++ src/main/java/com/github/vladimirpokhodnya/tasktracker/config/MailConfig.java
@@ -0,0 +1,27 @@
+package com.github.vladimirpokhodnya.tasktracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender(SpringMailProperties mailProperties) {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(mailProperties.getHost());
+        mailSender.setPort(mailProperties.getPort());
+        mailSender.setUsername(mailProperties.getUsername());
+        mailSender.setPassword(mailProperties.getPassword());
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", mailProperties.getProtocol());
+        props.put("mail.smtp.auth", mailProperties.getAuth());
+        props.put("mail.smtp.starttls.enable", mailProperties.getStarttls());
+        props.put("mail.debug", mailProperties.getDebug());
+        return mailSender;
+    }
+}
